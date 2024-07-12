bdea83ac36ccbf22d2393c6067bdc3c3b0e5aa2b
JReynaldoDelRio <reynaldodelriocontreras@gmail.com>
2024-07-12 15:51:13 -0500

    primerSpring

---
+++ chatiTienda/src/main/java/com/CorpDelRio/chatiTienda/ChatiTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.CorpDelRio.chatiTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatiTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatiTiendaApplication.class, args);
+	}
+
+}
